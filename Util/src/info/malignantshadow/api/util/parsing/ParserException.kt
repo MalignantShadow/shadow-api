493af3a754c8050a51d1e630e463fca47130ba00
Shad0w <malignant.shadowborne@gmail.com>
2018-01-05 16:03:46 -0800

    Remove deprecated Parser in favor of Tokenizer

--- Util/src/info/malignantshadow/api/util/parsing/ParserException.kt
+++
@@ -1,9 +0,0 @@
-package info.malignantshadow.api.util.parsing
-
-class ParserException(message: String, val parser: Parser) : RuntimeException(message) {
-
-    override val message: String
-        get() =
-                "${super.message} at character ${parser.index} (${parser.line}:${parser.lineIndex})"
-
-}
