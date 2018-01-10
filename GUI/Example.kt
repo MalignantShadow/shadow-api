2f36dd2213d7fbeb8036297c097b03b3e2d669c8
Shad0w <malignant.shadowborne@gmail.com>
2018-01-10 01:29:42 -0800

    Add README and simple example

---
+++ GUI/Example.kt
@@ -0,0 +1,45 @@
+import info.malignantshadow.api.gui.MainShell
+import info.malignantshadow.api.gui.composite
+import info.malignantshadow.api.gui.formData
+import info.malignantshadow.api.gui.formLayout
+import org.eclipse.swt.SWT
+import org.eclipse.swt.graphics.Point
+import org.eclipse.swt.layout.FormAttachment
+
+class ExampleShell : MainShell("example", "Example Shell") {
+
+    override val defaultSize = Point(500, 300)
+
+    override fun createContents() {
+        composite(this.shell) {
+            layout = formLayout {
+                spacing = 5
+                marginHeight = 5
+                marginWidth = 5
+            }
+
+            label(SWT.CENTER) {
+                text = "Welcome!"
+                layoutData = formData {
+                    top = FormAttachment(0)
+                    left = FormAttachment(0)
+                    right = FormAttachment(100)
+                }
+            }
+
+            button(SWT.PUSH) {
+                text = "Close Window"
+                layoutData = formData {
+                    bottom = FormAttachment(100)
+                    right = FormAttachment(100)
+                }
+                addListener(SWT.Selection, { super.end() })
+            }
+        }
+    }
+
+}
+
+fun main(args: Array<String>) {
+    ExampleShell().start()
+}
---
+++ GUI/README.md
@@ -0,0 +1,9 @@
+# ShadowAPI/GUI
+This module contains helper classes and functions for [SWT](https://www.eclipse.org/swt/).
+
+## DSL
+Most of the module is a DSL for creating SWT views.
+
+### Example
+If you run the [example code](Example.kt), this is the result: 
+![example window](ExampleWindow.png)
