6b35177c9cde2ddbc3bb430da93ff9149f1d817f
Shad0w <malignant.shadowborne@gmail.com>
2018-01-15 00:22:56 -0800

    Add a typealias for a simple command context

--- Commands/src/info/malignantshadow/api/commands/simple/SimpleCommandManager.kt
+++ Commands/src/info/malignantshadow/api/commands/simple/SimpleCommandManager.kt
@@ -1,8 +1,11 @@
 package info.malignantshadow.api.commands.simple
 
+import info.malignantshadow.api.commands.CommandContext
 import info.malignantshadow.api.commands.CommandManager
 import info.malignantshadow.api.commands.CommandSender
 import info.malignantshadow.api.util.build
+
+typealias SimpleCommandContext = CommandContext<SimpleCommand, CommandSender>
 
 class SimpleCommandManager : CommandManager<SimpleCommand, CommandSender>() {
 
