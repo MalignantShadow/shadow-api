77c34ae61460f860feb0cc53fb83b9b7e43618a2
Shad0w <malignant.shadowborne@gmail.com>
2018-01-15 00:20:08 -0800

    Spread operators are important, kids

--- Commands/src/info/malignantshadow/api/commands/CommandSender.kt
+++ Commands/src/info/malignantshadow/api/commands/CommandSender.kt
@@ -3,9 +3,9 @@ package info.malignantshadow.api.commands
 open class CommandSender {
 
     fun print(o: Any?) = print(o.toString())
-    open fun print(message: String, vararg args: Any?) = println(message.format(args))
+    open fun print(message: String, vararg args: Any?) = println(message.format(*args))
 
     fun printErr(o: Any?) = printErr(o.toString())
-    open fun printErr(message: String, vararg args: Any?) = println(message.format(args))
+    open fun printErr(message: String, vararg args: Any?) = println(message.format(*args))
 
 }
